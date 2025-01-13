c06ad8dbaad7253f6569a66b08ad679ab3734135
Wranitz <niteshpaudel96@gmail.com>
2025-01-13 12:03:55 +0000

    ran the main app by configuring url patterns, templates, view now, need to add huggingface transformer model

--- sysapp/sysapp/settings.py
+++ sysapp/sysapp/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'sysapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'sysapp/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
