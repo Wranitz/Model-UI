e8496e55a815cbb5f943382a6bcf4c857ef8ddb1
Wranitz <niteshpaudel96@gmail.com>
2025-01-22 13:55:08 +0000

    Need to setup JS, added some comments to huggingface file

--- sysapp/sysapp/huggingface.py
+++ sysapp/sysapp/huggingface.py
@@ -1,6 +1,46 @@
-from transformers import pipeline
+'''
+    1. Need to use multi model for different tasks.
+    2. First is text to text model for general purpose.
+    3. Second is text to text model for programming and math problems.
+    4. Third is Image to text model for general purpose.
+    5. Fourth is Image to text model for programming and math problems.
+    6. Fifth is Image to Image model for Image enhancement.
+    7. Sixth is Image to Image model for Image generation.
+    8. Seventh is Image to Video model for Video generation.
+    9. Partition the defferent phases of code execution for running multiple models.
+'''
 
-classifier = pipeline('sentiment-analysis')
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
-res = classifier('We are very happy to show you the 🤗 Transformers library.')
-print(res)
+model_name = "Qwen/Qwen2.5-0.5B-Instruct"
+
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    torch_dtype="auto",
+    device_map="auto"
+)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+prompt = "What is a Quick short algorithm? Give example."
+messages = [
+    {"role": "system", "content": "You are AI chatbot, You are a helpful assistant for Math and programming problems."},
+    {"role": "user", "content": prompt}
+]
+text = tokenizer.apply_chat_template(
+    messages,
+    tokenize=False,
+    add_generation_prompt=True
+)
+model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
+
+generated_ids = model.generate(
+    **model_inputs,
+    max_new_tokens=512
+)
+generated_ids = [
+    output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
+]
+
+response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+
+print(response)
