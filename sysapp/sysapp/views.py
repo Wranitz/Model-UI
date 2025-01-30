8dfd04c0092edd8ccd5e80898b07f12711f38253
Wranitz <niteshpaudel96@gmail.com>
2025-01-30 16:21:23 +0000

    Now need to work on views and JS for first phase of testing the whole LLM pipeline to webapp

--- sysapp/sysapp/views.py
+++ sysapp/sysapp/views.py
@@ -1,4 +1,30 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from my_django_project.settings import llm_1, llm_2
 
 def home(request):
     return render(request, 'home.html')
+
+def generate_text(request):
+    if request.method == 'POST':
+        prompt = request.POST['prompt']
+        response = llm_1(prompt)[0]['generated_text']
+        return JsonResponse({'response': response})
+    return JsonResponse({'response': 'Invalid request'})
+
+def classify_text(request):
+    if request.method == 'POST':
+        text = request.POST['text']
+        response = llm_2(text)
+        return JsonResponse({'response': response})
+    return JsonResponse({'response': 'Invalid request'})
+
+def generate_text_from_image(request):
+    return JsonResponse({'response': 'Not implemented yet'})
+
+def generate_image_from_text(request):
+    return JsonResponse({'response': 'Not implemented yet'})
+
+def generate_image_from_image(request):
+    return JsonResponse({'response': 'Not implemented yet'})
+
